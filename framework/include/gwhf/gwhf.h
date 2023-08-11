ea87fa1c1a47e6a93b1a99e5e16a550e51f87c76
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2023-08-11 19:59:30 +0700

    tmp_save: !rebase: tmp save
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- framework/include/gwhf/gwhf.h
+++ framework/include/gwhf/gwhf.h
@@ -38,17 +38,11 @@ struct gwhf_init_arg {
 	};
 };
 
-#ifdef GWHF_INTERNAL
 struct gwhf_internal;
-#endif
 
 struct gwhf {
 	volatile bool		stop;
-#ifdef GWHF_INTERNAL
 	struct gwhf_internal	*internal;
-#else
-	void			*internal;
-#endif
 	struct gwhf_init_arg	init_arg;
 };
 
