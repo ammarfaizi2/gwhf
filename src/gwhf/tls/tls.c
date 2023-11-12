2090089c3f1a8f2a26f069e7912d6d8aca27e419
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2023-11-13 03:35:54 +0700

    save tls
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/gwhf/tls/tls.c
+++ src/gwhf/tls/tls.c
@@ -23,6 +23,128 @@ static void key_to_pem(EVP_PKEY *key, uint8_t **key_bytes, size_t *key_size);
 static int load_ca(const char *ca_key_path, EVP_PKEY **ca_key, const char *ca_crt_path, X509 **ca_crt);
 static void print_bytes(uint8_t *data, size_t size);
 
+int gwhf_tls_gen_key_rsa(EVP_PKEY **out_key, unsigned bits)
+{
+	*out_key = EVP_RSA_gen(bits);
+	if (!*out_key)
+		return -1;
+
+	return 0;
+}
+
+int gwhf_tls_gen_x509_csr(X509_REQ **out_req, EVP_PKEY *key,
+			  struct gwhf_tls_csr_txt_entry *entries,
+			  size_t nr_entries)
+{
+	X509_NAME *name;
+	size_t i;
+
+	*out_req = X509_REQ_new();
+	if (!*out_req)
+		return -1;
+
+	X509_REQ_set_pubkey(*out_req, *key);
+	name = X509_REQ_get_subject_name(*req);
+	for (i = 0; i < nr_entries; i++) {
+		struct gwhf_tls_csr_txt_entry *e = &entries[i];
+		const unsigned char *val = (const unsigned char *)e->value;
+		int ret;
+
+		ret = X509_NAME_add_entry_by_txt(name, e->field, MBSTRING_ASC,
+						 val, -1, -1, 0);
+		if (!ret)
+			goto out_err;
+	}
+
+	/*
+	 * Self-sign the request to prove that we posses the key.
+	 */
+	if (!X509_REQ_sign(*out_req, *key, EVP_sha256()))
+		goto out_err;
+
+	return 0;
+
+out_err:
+	X509_REQ_free(*out_req);
+	*out_req = NULL;
+	return -1;
+}
+
+int gwhf_tls_signed_key_pair(EVP_PKEY **out_key, X509 **out_crt,
+			     EVP_PKEY *ca_key, X509 *ca_crt, unsigned bits,
+			     struct gwhf_tls_csr_txt_entry *csr_entries,
+			     size_t nr_csr_entries)
+{
+	EVP_PKEY *key = NULL;
+	X509 *crt = NULL;
+	int ret;
+
+	ret = gwhf_tls_gen_key_rsa(&key, bits);
+	if (ret)
+		goto out_err;
+
+	ret = gwhf_tls_gen_x509_csr(&crt, key, csr_entries, nr_csr_entries);
+	if (ret)
+		goto out_err;
+
+	ret = gwhf_tls_sign_x509_crt(ca_key, ca_crt, key, crt);
+	if (ret)
+		goto out_err;
+
+	*out_key = key;
+	*out_crt = crt;
+	return 0;
+
+out_err:
+	EVP_PKEY_free(key);
+	X509_free(crt);
+	return -1;
+}
+
+int generate_signed_key_pair(EVP_PKEY *ca_key, X509 *ca_crt, EVP_PKEY **key, X509 **crt)
+{
+	/* Generate the private key and corresponding CSR. */
+	X509_REQ *req = NULL;
+	if (!generate_key_csr(key, &req)) {
+		fprintf(stderr, "Failed to generate key and/or CSR!\n");
+		return 0;
+	}
+
+	/* Sign with the CA. */
+	*crt = X509_new();
+	if (!*crt) goto err;
+
+	X509_set_version(*crt, 2); /* Set version to X509v3 */
+
+	/* Generate random 20 byte serial. */
+	if (!generate_set_random_serial(*crt)) goto err;
+
+	/* Set issuer to CA's subject. */
+	X509_set_issuer_name(*crt, X509_get_subject_name(ca_crt));
+
+	/* Set validity of certificate to 2 years. */
+	X509_gmtime_adj(X509_get_notBefore(*crt), 0);
+	X509_gmtime_adj(X509_get_notAfter(*crt), (long)2*365*24*3600);
+
+	/* Get the request's subject and just use it (we don't bother checking it since we generated
+	 * it ourself). Also take the request's public key. */
+	X509_set_subject_name(*crt, X509_REQ_get_subject_name(req));
+	EVP_PKEY *req_pubkey = X509_REQ_get_pubkey(req);
+	X509_set_pubkey(*crt, req_pubkey);
+	EVP_PKEY_free(req_pubkey);
+
+	/* Now perform the actual signing with the CA. */
+	if (X509_sign(*crt, ca_key, EVP_sha256()) == 0) goto err;
+
+	X509_REQ_free(req);
+	return 1;
+err:
+	EVP_PKEY_free(*key);
+	X509_REQ_free(req);
+	X509_free(*crt);
+	return 0;
+}
+
 int gen_key(const char *ca_key_path, const char *ca_crt_path)
 {
 	/* Load CA key and cert. */
@@ -192,10 +314,7 @@ int load_ca(const char *ca_key_path, EVP_PKEY **ca_key, const char *ca_crt_path,
 	if (!BIO_read_filename(bio, ca_crt_path)) goto err;
 	*ca_crt = PEM_read_bio_X509(bio, NULL, NULL, NULL);
 	if (!*ca_crt) goto err;
-	BIO_free_all(bio);
-
-	/* Load CA private key. */
-	bio = BIO_new(BIO_s_file());
+	BIO_free_all(bio);BIO_free_all(bio);le());
 	if (!BIO_read_filename(bio, ca_key_path)) goto err;
 	*ca_key = PEM_read_bio_PrivateKey(bio, NULL, NULL, NULL);
 	if (!ca_key) goto err;
--- src/gwhf/tls/tls.h
+++ src/gwhf/tls/tls.h
@@ -2,6 +2,14 @@
 #ifndef GWHF__TLS__TLS_H
 #define GWHF__TLS__TLS_H
 
-int gen_key(const char *ca_key, const char *ca_crt);
+struct gwhf_tls_csr_txt_entry {
+	const char *field;
+	const char *value;
+};
+
+int gwhf_tls_gen_key_rsa(EVP_PKEY **out_key, unsigned bits);
+int gwhf_tls_gen_csr(X509_REQ **out_req, EVP_PKEY *key,
+		     struct gwhf_tls_csr_txt_entry *entries,
+		     size_t nr_entries);
 
 #endif /* #ifndef GWHF__TLS__TLS_H */
